4fb4b045f06bdc47880dd15c8e1cabb9c85f67ca
Mateo Álvarez Duque <mateoad18@educastur.es>
2024-10-07 19:44:55 +0200

    exclusion mutua2

--- src/main/java/Unidad2/Ejemplo9/Contador.java
+++ src/main/java/Unidad2/Ejemplo9/Contador.java
@@ -1,12 +1,19 @@
 package Unidad2.Ejemplo9;
 
 public class Contador {
+
     private int n;
-    public Contador(int n){
-        this.n=n;
+
+    public Contador(int n) {
+        this.n = n;
     }
-    public void inc(){
+
+    public synchronized void inc() {
         n++;
+    }
+
+    public int get() {
+        return n;
     }
 
 }
